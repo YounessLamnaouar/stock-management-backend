878c3321d7344ca9e004cbe58a34e00fa3b84d39
Youness Lamnaouar <youneslam123@gmail.com>
2026-08-16 00:42:15 +0100

    Add early CORS preflight handling in index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,27 @@
 <?php
 
+// Handle CORS early for all incoming requests (including preflight OPTIONS)
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+} else {
+    header("Access-Control-Allow-Origin: *");
+}
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    } else {
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin");
+    }
+    http_response_code(200);
+    exit(0);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
